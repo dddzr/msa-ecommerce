f155b5244d6d19a154ca831cc0b0cf2d716344e8
lsm <sumin1113@saerom.co.kr>
2025-03-14 16:45:15 +0900

    Squashed 'user_service/' changes from b103e8b..79d3b01
    79d3b01 private함수 적용, test코드 적용
    
    git-subtree-dir: user_service
    git-subtree-split: 79d3b01e857edae4543c56c3d63ce1f3d8fd218d

--- src/main/java/com/example/user_service/entity/User.java
+++ src/main/java/com/example/user_service/entity/User.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
+// @Table(name = "users") user은 예약어라서 h2 DB로 테스트 할 때 users로 사용
 @Data
 public class User {
     @Id
@@ -16,7 +18,7 @@ public class User {
     private String email;
     private String nickname;
     private String password;
-    private String profile_picture;
+    private String profilePicture;
     private String createdAt;
     private String updatedAt;
     private String status;
